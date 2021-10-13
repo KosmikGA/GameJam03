80211f4b098180fd065e4d41165261d43c9d229f
Caleb Ford <gameart.kosmik@gmail.com>
2021-10-14 12:10:13 +1300

    Greyboxed Environment
    Environment created, ready to rebuild with game sprites

---
+++ Assets/TN/Scripts/PlayerMovment.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovment : MonoBehaviour
+{
+
+    public float speed = 1f;
+    public float jumpForce = 1f;
+    private float moveInput;
+
+    private Rigidbody2D rb;
+
+    private bool facingRight = true;
+
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;   //i dont know but if you find out please tell me//
+
+    private int extraJumps;
+    public int extraJumpsValue;
+
+    void Start()
+    {
+        extraJumps = extraJumpsValue;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    private void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround); //please someone tell me what is ground//
+
+        moveInput = Input.GetAxis("Horizontal");
+        //Debug.Log(moveInput);
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
+        if (facingRight == false && moveInput > 0)
+        {
+            Flip();
+        }
+        else if (facingRight == true && moveInput < 0)
+        {
+            Flip();
+        }
+    }
+
+
+    void Update()
+    {
+        if (isGrounded == true)
+        {
+            extraJumps = extraJumpsValue;
+        }
+
+        if(Input.GetKeyDown(KeyCode.W) && extraJumps > 0)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+            extraJumps--;
+        }
+        else if(Input.GetKeyDown(KeyCode.W) && extraJumps == 0 && isGrounded == true)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+    }
+
+
+void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 Rotate = transform.localScale;
+        Rotate.x *= -1;
+        transform.localScale = Rotate;
+    }
+}

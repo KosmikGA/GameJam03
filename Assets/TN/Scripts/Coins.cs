61491b0f68c3141f69f28f530eb234180c42ab37
Tom <shyfoxsounds@gmail.com>
2021-10-15 13:52:55 +1300

    Coin || Main Menu
    added coins that add to a score
    made a very basic main menu nothing special

---
+++ Assets/TN/Scripts/Coins.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coins : MonoBehaviour
+{
+    public ScoreManager manager = null;
+    public string player = null;
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Coins"))
+        {
+            manager.AddPoint();
+        }
+    }
+}
---
+++ Assets/TN/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject GameOver;
+    public ScoreManager scoreManager;
+
+    void Start()
+    {
+        Time.timeScale = 1;
+    }
+
+   public void GameFail()
+    {
+        scoreManager.GameEnd();
+
+        GameOver.SetActive(true);
+        Time.timeScale = 1;
+    }
+}
